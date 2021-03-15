c72fb42d54198a7df2e8e3364e03026338e5c82d
DESKTOP-K9TD032\ITPS <kelly16012@gmail.com>
2021-03-15 17:53:19 +0900

    2021.03.15

---
+++ src/chapter2/Ex1.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		//문자
+		char value;
+		
+		//정수
+		int number;
+		
+		//실수
+		double data;
+		
+		//논리
+		boolean val1;
+		
+	}
+
+}
